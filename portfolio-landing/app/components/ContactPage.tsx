cb9f4770d247bffd875842f9ccbffc197bd15d1d
Jose Latapiatt <jose.latapiatt@proton.me>
2024-06-12 05:34:42 -0400

    Cambios en el contact con el tiempo

--- portfolio-landing/app/components/ContactPage.tsx
+++ portfolio-landing/app/components/ContactPage.tsx
@@ -8,7 +8,13 @@ interface ContactPageProps {
     style: CSSProperties;
 }
 const ContactPage: React.FC<ContactPageProps> = ({ contacts, style }) => {
-    const [time1, setTime] = useState<Date>(new Date());
+    const [isVisible1, setIsVisible1] = useState<boolean>(true);
+    const [isVisible2, setIsVisible2] = useState<boolean>(false);
+    const [isVisible3, setIsVisible3] = useState<boolean>(false);
+    const [isVisible4, setIsVisible4] = useState<boolean>(false);
+    const [isVisible5, setIsVisible5] = useState<boolean>(false);
+    const [isVisible6, setIsVisible6] = useState<boolean>(false);
+    const [time1, setTime1] = useState<Date>(new Date());
     const [time2, setTime2] = useState<Date>(new Date((new Date).getTime() + 15 * 1000));
     const [time3, setTime3] = useState<Date>(new Date((new Date).getTime() + 25 * 1000));
     const [time4, setTime4] = useState<Date>(new Date((new Date).getTime() + 26 * 1000));
@@ -17,24 +23,61 @@ const ContactPage: React.FC<ContactPageProps> = ({ contacts, style }) => {
     const times: Date[] = [time3, time4, time5];
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(new Date()),
-            setTime2(new Date((new Date).getTime() + 15 * 1000)),
-            setTime3(new Date((new Date).getTime() + 25 * 1000));
-            setTime4(new Date((new Date).getTime() + 26 * 1000));
-            setTime5(new Date((new Date).getTime() + 27 * 1000));
-            setTime6(new Date((new Date).getTime() + 37 * 1000));
-        }, 60000);
+        // const intervalTimer = setInterval(() => {
+        //     setTime1(new Date());
+        //     setTime2(new Date((new Date).getTime() + 15 * 1000)),
+        //     setTime3(new Date((new Date).getTime() + 25 * 1000));
+        //     setTime4(new Date((new Date).getTime() + 26 * 1000));
+        //     setTime5(new Date((new Date).getTime() + 27 * 1000));
+        //     setTime6(new Date((new Date).getTime() + 37 * 1000));
+        // }, 60000);
         
-        return () => clearInterval(timer);
+        const timeOutTimer1 = setTimeout(() => {
+            setTime1(new Date());
+            setIsVisible1(true);
+        }, 1000);
+
+        const timeOutTimer2 = setTimeout(() => {
+            setTime2(new Date());
+            setIsVisible2(true);
+        }, 6000);
+
+        const timeOutTimer3 = setTimeout(() => {
+            setTime3(new Date());
+            setIsVisible3(true);
+        }, 9000);
+
+        const timeOutTimer4 = setTimeout(() => {
+            setTime4(new Date());
+            setIsVisible4(true);
+        }, 9000);
+
+        const timeOutTimer5 = setTimeout(() => {
+            setTime5(new Date());
+            setIsVisible5(true);
+        }, 9000);
+
+        const timeOutTimer6 = setTimeout(() => {
+            setTime6(new Date());
+            setIsVisible6(true);
+        }, 12000);
+
+        return () => {
+            clearTimeout(timeOutTimer1);
+            clearTimeout(timeOutTimer2);
+            clearTimeout(timeOutTimer3);
+            clearTimeout(timeOutTimer4);
+            clearTimeout(timeOutTimer5);
+            clearTimeout(timeOutTimer6);
+        }
     }, []);
 
     return (
         <div className='flex justify-center items-center p-10'>
             <div className="mockup-phone border border-primary mx-auto min-w-fit w-2/3 ">
                 <div className="camera"></div> 
-                <div className='bg-cover bg-no-repeat display p-7' style={style}>
-                    <div className="chat chat-start">
+                <div className='bg-cover bg-no-repeat display min-h-[1190px] p-7' style={style}>
+                    <div className={"chat chat-start " + (isVisible1 ? 'grid' : 'hidden')}>
                         <div className="chat-image avatar">
                             <div className="w-10 rounded-full">
                                 <img alt="Imagen de perfil" src="/self.jpg" />
@@ -49,7 +92,7 @@ const ContactPage: React.FC<ContactPageProps> = ({ contacts, style }) => {
                             Delivered
                         </div>
                     </div>
-                    <div className="chat chat-end">
+                    <div className={"chat chat-end " + (isVisible2 ? 'grid' : 'hidden')}>
                         <div className="chat-image avatar">
                             <div className="w-10 rounded-full">
                                 <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -64,10 +107,13 @@ const ContactPage: React.FC<ContactPageProps> = ({ contacts, style }) => {
                             Seen at {time2.toLocaleTimeString('en-US')}
                         </div>
                     </div>
+                    
                     {contacts.map((contact, index) => (
-                        <ContactItem key={contact.id} contact={contact} time={times[index]}></ContactItem>
+                        <div className={isVisible3 ? 'grid' : 'hidden'}>
+                            <ContactItem key={contact.id} contact={contact} time={times[index]}></ContactItem>
+                        </div>
                     ))}
-                    <div className="chat chat-end">
+                    <div className={"chat chat-end " + (isVisible6 ? 'grid' : 'hidden')}>
                         <div className="chat-image avatar">
                             <div className="w-10 rounded-full">
                                 <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
